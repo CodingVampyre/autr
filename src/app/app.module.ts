613f7d9814c57697c09fcc45b2b1807424141e68
Skraun Greenspot <skraun@pop-os.localdomain>
2019-09-16 19:45:15 +0200

    add a route, add button design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'projects', component: ProjectListComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
