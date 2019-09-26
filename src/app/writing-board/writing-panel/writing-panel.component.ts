b9daf8f231b06ed2139a0abdd2e48a45d0c9261d
Skraun Greenspot <skraun@pop-os.localdomain>
2019-09-26 11:34:09 +0200

    added listener for text

--- src/app/writing-board/writing-panel/writing-panel.component.ts
+++ src/app/writing-board/writing-panel/writing-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NovelProjectProviderService } from 'src/app/novel-project-provider.service';
 
 @Component({
   selector: 'app-writing-panel',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WritingPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private novelService: NovelProjectProviderService,
+  ) { }
+
+  private model: string = "this is my novel text";
 
   ngOnInit() {
+  }
+
+  onBlur() {
+    console.log(this.model);
+  }
+
+  onInput() {
+    console.log(this.model);
   }
 
 }
