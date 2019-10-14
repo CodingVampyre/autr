4a3485829d82f0e5e371c2b7b035e261f4e9c5e8
CodingVampyre <tovuko@hotmail.de>
2019-10-14 20:13:52 +0200

    add a few commas and semikolons in main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
         }
     })
 
@@ -18,23 +18,23 @@ function createWindow() {
         url.format({
             pathname: path.join(__dirname, `/dist/index.html`),
             protocol: "file:",
-            slashes: true
+            slashes: true,
         })
     );
     // Open the DevTools.
     mainWindow.webContents.openDevTools()
 
     mainWindow.on('closed', function () {
-        mainWindow = null
+        mainWindow = null;
     });
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', function () {
-    if (mainWindow === null) createWindow()
+    if (mainWindow === null) createWindow();
 });
