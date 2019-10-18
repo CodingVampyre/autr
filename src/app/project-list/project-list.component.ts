82f3461aa224a796cfcd75794003f6dcd050d203
CodingVampyre <tovuko@hotmail.de>
2019-10-19 00:43:42 +0200

    add pouchdb as main database

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ * this code is licensed under the MIT-License
+ * <https://opensource.org/licenses/MIT>
+ * AUTR - 2019
+ *
+ * Created by CodingVampyre <tobiaskavse@hotmail.de>
+ */
+
+import { Component, OnInit } from "@angular/core";
+import { DatabaseService } from "../services/database.service";
 
 @Component({
-  selector: 'app-project-list',
-  templateUrl: './project-list.component.html',
-  styleUrls: ['./project-list.component.less']
+  selector: "app-project-list",
+  templateUrl: "./project-list.component.html",
+  styleUrls: ["./project-list.component.less"]
 })
 export class ProjectListComponent implements OnInit {
+  constructor(private readonly db: DatabaseService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 }
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -7,12 +7,44 @@
  */
 
 import { Injectable } from '@angular/core';
-import * as Level from 'level';
+import PouchDB from 'pouchdb';
+import {Novel} from '../data-models/novel.interface';
 
+/** manages persistent storage with level */
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
 
+  /** database instance */
+  private db = new PouchDB('autr');
+
+  /** default constructor */
   constructor() { }
+
+  /**
+   * stores a novel into the database
+   * @param key {string} the key to store the database to
+   * @param novel {Novel} the book to store
+   * @returns {Promise<void>}
+   */
+  public async storeNovel(key: string, novel: Novel): Promise<void> {
+    this.db.put({_id: 'novel:' + key, novel});
+  }
+
+  /**
+   * fetches a single novel
+   * @param key {string}
+   * @returns {Promise<Novel | undefined>}
+   */
+  public async fetchNovel(key: string): Promise<Novel | undefined> {
+    return await this.db.get(key);
+  }
+
+  public async storeTestData<T>(key: string, data: T) {
+    await this.db.put({_id: key, data});
+  }
+  public async retrieveTestData(key: string) {
+    return await this.db.get(key);
+  }
 }
