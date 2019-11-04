c8edf3fb29e4d0312dd2b1e50dfca1ad5d414651
CodingVampyre <tovuko@hotmail.de>
2019-11-04 23:13:46 +0100

    add design to export menu

--- src/app/export-menu/export-menu.component.ts
+++ src/app/export-menu/export-menu.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+// const { dialog } = require('electron').remote;
+
 
 @Component({
-  selector: 'app-export-menu',
-  templateUrl: './export-menu.component.html',
-  styleUrls: ['./export-menu.component.less']
+	selector: 'app-export-menu',
+	templateUrl: './export-menu.component.html',
+	styleUrls: ['./export-menu.component.less']
 })
 export class ExportMenuComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	onClickExportNovelAsJSON() {
+		//const path: string[] = dialog.showOpenDialogSync({
+		//	title: "Export JSON to...",
+		//});
+	}
 
 }
