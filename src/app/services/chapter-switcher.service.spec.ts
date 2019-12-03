87f41ba21d53e1699757aca815a098f723aafa90
Tobias Kavsek <tobiaskavsek@hotmail.de>
2019-12-03 13:03:39 +0100

    memorize cursor position now

--- src/app/services/chapter-switcher.service.spec.ts
+++ src/app/services/chapter-switcher.service.spec.ts
@@ -3,10 +3,10 @@ import { TestBed } from '@angular/core/testing';
 import { ChapterSwitcherService } from './chapter-switcher.service';
 
 describe('ChapterSwitcherService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+	beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: ChapterSwitcherService = TestBed.get(ChapterSwitcherService);
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		const service: ChapterSwitcherService = TestBed.get(ChapterSwitcherService);
+		expect(service).toBeTruthy();
+	});
 });
--- src/app/services/chapter-switcher.service.ts
+++ src/app/services/chapter-switcher.service.ts
@@ -1,26 +1,35 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import {NovelProjectProviderService} from './novel-project-provider.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChapterSwitcherService {
 
-  public switchToChapterEmitter = new EventEmitter<{toChapter: number, toScene: number}>();
-  public saveTextEmitter = new EventEmitter<{chapter: number, scene: number}>();
-  public currentChapter: number = 0;
-  public currentScene: number = 0;
+	public switchToChapterEmitter = new EventEmitter<{toChapter: number, toScene: number}>();
+	public saveTextEmitter = new EventEmitter<{chapter: number, scene: number}>();
+	public currentChapter: number = 0;
+	public currentScene: number = 0;
 
   // drag and drop of new stuff
   public dragContent: DropType = DropType.NONE;
 
-  constructor() { 
-    this.switchToChapterEmitter.subscribe((event) => {
-      this.currentChapter = event.toChapter;
-      this.currentScene = event.toScene;
-    });
-  }
+	constructor(
+		private novelProjectProviderService: NovelProjectProviderService,
+	) {
+		this.switchToChapterEmitter.subscribe((event) => {
+			this.currentChapter = event.toChapter;
+			this.currentScene = event.toScene;
+
+			this.novelProjectProviderService.getNovel().cursor.currentChapter = event.toChapter;
+			this.novelProjectProviderService.getNovel().cursor.currentScene = event.toScene;
+		});
+	}
 }
 
 export enum DropType {
-  NONE, CHAPTER, SCENE, NOTE
+	NONE,
+	CHAPTER,
+	SCENE,
+	NOTE
 }
