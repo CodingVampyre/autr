0f40774216133397b8ac317d17523d2ec0a15052
Tobias Kavsek <tobiaskavsek@hotmail.de>
2019-12-24 17:53:49 +0100

    :wrench: preparation for removing spawning

--- src/app/components/writing-board/writing-board.component.ts
+++ src/app/components/writing-board/writing-board.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { NovelProviderService } from '../../services/novel-provider.service';
 import { NotificationService } from '../../services/notification.service';
-import { ChapterSwitcherService } from '../../services/chapter-switcher.service';
 
 @Component({
 	selector: 'app-writing-board',
 	templateUrl: './writing-board.component.html',
-	styleUrls: ['./writing-board.component.less']
+	styleUrls: ['./writing-board.component.less'],
 })
 export class WritingBoardComponent implements OnInit {
 
@@ -17,9 +16,9 @@ export class WritingBoardComponent implements OnInit {
 		private notificationService: NotificationService,
 	) { }
 
-	async ngOnInit() {
+	public async ngOnInit() {
 		// return back if no novel is loaded
-		if (this.novelProvider.getNovel() == null) {
+		if (this.novelProvider.getNovel() === undefined) {
 			this.notificationService.newNotificationEmitter.emit('no novel was loaded. Return to project list...');
 			await this.router.navigate(['projects']);
 		}
