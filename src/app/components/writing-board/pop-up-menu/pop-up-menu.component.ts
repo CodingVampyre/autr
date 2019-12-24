1abbde31113000f29d9542a3eb14f8ef1fbf3939
Tobias Kavsek <tobiaskavsek@hotmail.de>
2019-12-24 15:48:46 +0100

    :wrench: edit pop up menu code to fulfil linting

--- src/app/components/writing-board/pop-up-menu/pop-up-menu.component.ts
+++ src/app/components/writing-board/pop-up-menu/pop-up-menu.component.ts
@@ -1,72 +1,78 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
-import { NovelProviderService } from 'src/app/services/novel-provider.service';
+import { NovelProviderService } from '../../../services/novel-provider.service';
 import { EventEmitter } from '@angular/core';
-import { ChapterSwitcherService } from 'src/app/services/chapter-switcher.service';
+import { ChapterSwitcherService } from '../../../services/chapter-switcher.service';
 
 @Component({
 	selector: 'app-pop-up-menu',
 	templateUrl: './pop-up-menu.component.html',
-	styleUrls: ['./pop-up-menu.component.less']
+	styleUrls: ['./pop-up-menu.component.less'],
 })
-export class PopUpMenuComponent implements OnInit {
+export class PopUpMenuComponent {
 
-	@Input() chapterNr: number;
-	@Input() sceneNr: number;
-	@Input() context: 'chapter' | 'scene';
+	@Input() public chapterNr: number;
+	@Input() public sceneNr: number;
+	@Input() public context: 'chapter' | 'scene';
 
-	@Output() destroyEmitter: EventEmitter<void> = new EventEmitter();
+	@Output() public destroyEmitter: EventEmitter<void> = new EventEmitter();
 
 	constructor(
 		private novelProvider: NovelProviderService,
 		private chapterSwitcher: ChapterSwitcherService,
 	) { }
 
-	renameChapter(chapterNewName: string) {
+	public renameChapter(chapterNewName: string) {
 		this.novelProvider.renameChapter(this.chapterNr, chapterNewName);
 	}
 
-	renameScene(sceneNewName: string) {
+	public renameScene(sceneNewName: string) {
 		this.novelProvider.renameScene(this.chapterNr, this.sceneNr, sceneNewName);
 	}
 
-	ngOnInit() {
-	}
+	public onClickDeleteChapter(chapterNr: number) {
+		const hasNovelNoMoreChapters = this.novelProvider.getNovel().chapters.length <= 0;
+		const isPreviousChapterExisting = this.novelProvider.getNovel().chapters[chapterNr - 1] !== undefined;
+		const hasPreviousChapterScenes = this.novelProvider.getNovel().chapters[chapterNr - 1].scenes.length > 0;
+		const hasFirstChapterAnyScenes = this.novelProvider.getNovel().chapters[0].scenes.length > 0;
 
-	onClickDeleteChapter(chapterNr: number) {
 		this.novelProvider.deleteChapter(chapterNr);
 
-		if (this.novelProvider.getNovel().chapters.length <= 0) {
+		if (hasNovelNoMoreChapters) {
 			this.novelProvider.addChapter(0);
-			this.chapterSwitcher.switchToChapterEmitter.emit({toChapter: 0, toScene: 0});
-		} else if (this.novelProvider.getNovel().chapters[chapterNr - 1] != null && this.novelProvider.getNovel().chapters[chapterNr - 1].scenes.length > 0) {
-			this.chapterSwitcher.switchToChapterEmitter.emit({toChapter: chapterNr - 1, toScene: 0});
-		} else if (this.novelProvider.getNovel().chapters[0].scenes.length > 0) {
-			this.chapterSwitcher.switchToChapterEmitter.emit({toChapter: 0, toScene: 0});
+			this.chapterSwitcher.switchToChapterEmitter.emit({ toChapter: 0, toScene: 0});
+		} else if (isPreviousChapterExisting && hasPreviousChapterScenes) {
+			this.chapterSwitcher.switchToChapterEmitter.emit({ toChapter: chapterNr - 1, toScene: 0});
+		} else if (hasFirstChapterAnyScenes) {
+			this.chapterSwitcher.switchToChapterEmitter.emit({ toChapter: 0, toScene: 0});
 		} else {
 			this.novelProvider.addScene(0, 0);
-			this.chapterSwitcher.switchToChapterEmitter.emit({toChapter: 0, toScene: 0});
+			this.chapterSwitcher.switchToChapterEmitter.emit({ toChapter: 0, toScene: 0});
 		}
 
 		return this.destroyMe();
 	}
 
-	onClickDeleteScene(chapterNr: number, sceneNr: number) {
+	public onClickDeleteScene(chapterNr: number, sceneNr: number) {
+		const isPreviousSceneExisting = this.novelProvider.getNovel().chapters[chapterNr].scenes[sceneNr - 1] !== undefined;
+		const isPreviousChapterExisting = this.novelProvider.getNovel().chapters[chapterNr - 1] !== undefined;
+		const hasPreviousChapterScenes = this.novelProvider.getNovel().chapters[chapterNr - 1].scenes.length > 0;
+
 		this.novelProvider.deleteScene(chapterNr, sceneNr);
 
 		// switch to other scene
-		if (this.novelProvider.getNovel().chapters[chapterNr].scenes[sceneNr - 1] != null) {
-			this.chapterSwitcher.switchToChapterEmitter.emit({toChapter: chapterNr, toScene: sceneNr - 1});
-		} else if (this.novelProvider.getNovel().chapters[chapterNr - 1] != null && this.novelProvider.getNovel().chapters[chapterNr - 1].scenes.length > 0) {
-			this.chapterSwitcher.switchToChapterEmitter.emit({toChapter: chapterNr - 1, toScene: 0});
+		if (isPreviousSceneExisting) {
+			this.chapterSwitcher.switchToChapterEmitter.emit({ toChapter: chapterNr, toScene: sceneNr - 1});
+		} else if (isPreviousChapterExisting && hasPreviousChapterScenes) {
+			this.chapterSwitcher.switchToChapterEmitter.emit({ toChapter: chapterNr - 1, toScene: 0});
 		} else {
 			this.novelProvider.addScene(0, 0);
-			this.chapterSwitcher.switchToChapterEmitter.emit({toChapter: 0, toScene: 0});
+			this.chapterSwitcher.switchToChapterEmitter.emit({ toChapter: 0, toScene: 0});
 		}
 
 		this.destroyMe();
 	}
 
-	destroyMe() {
+	public destroyMe() {
 		this.destroyEmitter.emit();
 	}
 
