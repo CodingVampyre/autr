16f99c4ed91acffc995d8923ff25d03075bbfb8a
Tobias Kavsek <tobiaskavsek@hotmail.de>
2019-12-30 17:56:30 +0100

    :wrench: create tag lists

--- src/app/services/world-builder.service.ts
+++ src/app/services/world-builder.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Nanograph } from 'nanograph';
 import { DatabaseService } from './database.service';
+import { IImageTag } from '../data-models/image-tag.interface';
+import { ITag } from '../data-models/tag.interface';
 
 @Injectable({
 	providedIn: 'root',
@@ -13,4 +15,28 @@ export class WorldBuilderService {
 	constructor(
 		public databaseService: DatabaseService,
 	) { }
+
+	/**
+	 * list all characters
+	 * @return a list if Image Tags containing ids of all characters
+	 */
+	public listCharacters(): IImageTag[] {
+		return [];
+	}
+
+	/**
+	 * lists all places
+	 * @return a list of tags
+	 */
+	public listPlaces(): ITag[] {
+		return [];
+	}
+
+	/**
+	 * lists all objects
+	 * @return a list of objects
+	 */
+	public listObjects(): ITag[] {
+		return [];
+	}
 }
