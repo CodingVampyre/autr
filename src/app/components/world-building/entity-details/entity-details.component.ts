1452c72fb7457c31e9080b4d49e5bf98628636b7
Tobias Kavsek <tobiaskavsek@hotmail.de>
2020-01-05 02:02:56 +0100

    :wrench: edit world building components to fix bug with categories

--- src/app/components/world-building/entity-details/entity-details.component.ts
+++ src/app/components/world-building/entity-details/entity-details.component.ts
@@ -7,7 +7,7 @@ import { IKeyValueEntity } from '../../common/entity-descriptor/entity-descripto
 	templateUrl: './entity-details.component.html',
 	styleUrls: ['./entity-details.component.less'],
 })
-export class EntityDetailsComponent implements OnInit {
+export class EntityDetailsComponent {
 
 	/** contents */
 	@Input() public contents: IEntityCategory[];
@@ -21,13 +21,9 @@ export class EntityDetailsComponent implements OnInit {
 	/** the entities that currently can be edited */
 	private currentlySelectedEntities: IKeyValueEntity[];
 
-	public ngOnInit() {
-		if (this.currentlySelectedCategory !== undefined) {
-			this.currentlySelectedCategory = this.contents[0].category;
-			this.currentlySelectedEntities = this.contents[0].entity;
-		}
-	}
-
+	/**
+	 * @return a tag list of categories
+	 */
 	public listCategories(): ITag[] {
 		return this.contents.map((content) => content.category);
 	}
@@ -35,12 +31,16 @@ export class EntityDetailsComponent implements OnInit {
 	/**
 	 *
 	 * @param category
-	 * @todo take an object containing multiple arrays to let categories switch dynamically
 	 */
 	public onClickChangeCategory(category: ITag): void {
-		this.currentlySelectedCategory = category;
+		if (this.contents === undefined) { return; }
+
 		for (const entities of this.contents) {
-			if (entities.category.id === category.id) { this.currentlySelectedEntities = entities.entity; }
+			if (entities.category.id === category.id) {
+				this.currentlySelectedCategory = category;
+				this.currentlySelectedEntities = entities.entity;
+				return;
+			}
 		}
 	}
 
--- src/app/components/world-building/world-building.component.ts
+++ src/app/components/world-building/world-building.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { WorldBuilderService } from '../../services/world-builder.service';
-import { IEntityCategory } from './entity-details/entity-details.component';
+import { EntityDetailsComponent, IEntityCategory } from './entity-details/entity-details.component';
 import { v1 as UUID } from 'uuid';
 
 @Component({
@@ -9,6 +9,9 @@ import { v1 as UUID } from 'uuid';
 	styleUrls: ['./world-building.component.less'],
 })
 export class WorldBuildingComponent {
+
+	@ViewChild('entityDetailsComponent', { static: true, read: EntityDetailsComponent })
+	public entityDetailsComponent: EntityDetailsComponent;
 
 	public contentToEdit: IEntityCategory[];
 
@@ -25,19 +28,18 @@ export class WorldBuildingComponent {
 	/** creates a new character and sets thee focus on editing that one */
 	public onClickCreateCharacter(newCharacterName: string): void {
 		const newCharacterId = UUID();
+		const categoryTemplate = [
+			{ category: { id: 'appearance', text: 'appearance' }, entity: [] },
+			{ category: { id: 'mindset', text: 'mindset' }, entity: [] },
+			{ category: { id: 'story', text: 'story' }, entity: [] },
+		];
 
 		this.worldBuilderService.createCharacter({
 			id: newCharacterId,
 			name: newCharacterName,
 			imgUrl: 'http://nightmare.mit.edu/static/faces/4a58b263dff079c4c6f23a0ad8bba719.png',
-			data: [
-				{ category: { id: 'appearance', text: 'appearance' }, entity: [] },
-				{ category: { id: 'mindset', text: 'mindset' }, entity: [] },
-				{ category: { id: 'story', text: 'story' }, entity: [] },
-			],
+			data: categoryTemplate,
 		});
-
-		this.activateCharacter(newCharacterId);
 	}
 
 	public onLoadListCharacters() {
@@ -51,9 +53,16 @@ export class WorldBuildingComponent {
 	public activateCharacter(characterId: string) {
 		const character = this.worldBuilderService.retrieveCharacter(characterId);
 		if (character !== undefined) {
+
+			// set metadata
 			this.contentToEdit = character.data;
 			this.currentlySelectedCharacterName = character.name;
 			this.currentlySelectedCharacterId = character.id;
+
+			// select first category
+			const category = this.contentToEdit[0].category;
+			if (category !== undefined) { this.entityDetailsComponent.onClickChangeCategory(category); }
+
 		} else {
 			console.error('character not found');
 		}
