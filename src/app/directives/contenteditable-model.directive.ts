2492a2718250219f96c79605c77443d1deb79451
Tobias Kavsek <tobiaskavsek@hotmail.de>
2020-01-07 00:08:08 +0100

    :wrench: document directive and prevent images from getting dragged

--- src/app/directives/contenteditable-model.directive.ts
+++ src/app/directives/contenteditable-model.directive.ts
@@ -1,37 +1,64 @@
 import { Directive, Input, Output, ElementRef, EventEmitter, SimpleChanges } from '@angular/core';
 
+/** allows to edit text in a div and work with the edited text */
 @Directive({
-  selector: '[contenteditableModel]',
-  host: {
-    '(keyup)': 'onKeyup()',
-  }
+	selector: '[contenteditableModel]',
+	host: {
+		'(keyup)': 'onKeyup()',
+	},
 })
 export class ContenteditableModelDirective {
 
-  @Input('contenteditableModel') model: string;
-  @Output('contenteditableModelChange') update = new EventEmitter();
+	/** the model that is edited, comes from outside */
+	@Input('contenteditableModel') public model: string;
 
-  private lastViewModel: string;
+	/** gets called when the user types something */
+	@Output('contenteditableModelChange') public update = new EventEmitter();
 
-  constructor(
-    private elRef: ElementRef
-  ) { }
+	/** last view model used */
+	private lastViewModel: string;
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['model'] && changes['model'].currentValue !== this.lastViewModel) {
-      this.lastViewModel = this.model;
-      this.refreshView();
-    }
-  }
+	/**
+	 * default constructor
+	 * @param elRef allows to work with the contents of the element this directive is assigned to
+	 */
+	constructor(
+		private elRef: ElementRef,
+	) { }
 
-  onKeyup() {
-    const value = this.elRef.nativeElement.innerText;
-    this.lastViewModel = value;
-    this.update.emit(value);
-  }
+	/**
+	 * called when the content of the element changes
+	 * @param changes the changes that are made
+	 */
+	public ngOnChanges(changes: SimpleChanges) {
 
-  private refreshView() {
-    this.elRef.nativeElement.innerText = this.model;
-  }
+		// if new data are typed
+		if (changes.model && changes.model.currentValue !== this.lastViewModel) {
+
+			// store last model
+			this.lastViewModel = this.model;
+
+			// refresh displayed text
+			this.refreshView();
+		}
+	}
+
+	/** called when a key is lifted */
+	public onKeyup() {
+
+		// get typed text
+		const value = this.elRef.nativeElement.innerText;
+
+		// store written text
+		this.lastViewModel = value;
+
+		// notify about changes to outside
+		this.update.emit(value);
+	}
+
+	/** rewrite the text of the element */
+	private refreshView() {
+		this.elRef.nativeElement.innerText = this.model;
+	}
 
 }
