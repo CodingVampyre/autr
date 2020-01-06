e40943e524aeea864b7a08630d8e6f13e920e0bf
Tobias Kavsek <tobiaskavsek@hotmail.de>
2020-01-06 23:22:58 +0100

    :balloon: create button to delete entried in key value list

--- src/app/components/common/entity-descriptor/entity-descriptor.component.ts
+++ src/app/components/common/entity-descriptor/entity-descriptor.component.ts
@@ -13,6 +13,9 @@ export class EntityDescriptorComponent {
 		this.entity.push({ name: 'new entry', value: 'write something' });
 	}
 
+	public deleteEntry(index: number) {
+		this.entity.splice(index, 1);
+	}
 }
 
 export interface IKeyValueEntity { name: string; value: string; }
